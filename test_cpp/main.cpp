55e980e2944c1fd3d6c95b0d7fa9781338ac6aa6
Egor1807 <egor.tatischev1807@gmail.com>
2021-10-10 21:22:51 +0300

    1 commit

---
+++ test_cpp/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file main.cpp
+ * @author Egor Tatischev (egor.tatischev1807@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a, b;
+
+    cin >> a >>  b;
+
+    cout << a + b;
+}
